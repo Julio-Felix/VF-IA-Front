8522e51bea94f573a3fe50044dfddcb957f79727
julio_felix_deway <julio.felix@deway.com.br>
2022-05-25 16:08:03 -0300

    fix All

--- src/App.js
+++ src/App.js
@@ -42,47 +42,66 @@ export const options = {
   },
 };
 
-const labels = ['1980', '1990', '2000', '2005', '2010', '2015', '2020'];
+
 const coeA = 0.4
 const coeB = -388
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map((x) => coeA*x + coeB),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      type:'line',
-      fill: false,
-      showLine: false, //<- set this
-      pointBackgroundColor: 'black',
-      pointRadius: 5,
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
 
 export default function App() {
   const [coefA, setCoefA] = useState(0)
   const [coefB, setCoefB] = useState(0)
   const [newDataX, setNewDataX] = useState(0)
   const [newDataY, setNewDataY] = useState(0)
+  const [apprentice, setApprentice] = useState(0)
+  const [iterate, setIterate] = useState(0)
 
   const [dataBack, setDataBack] = useState([]);
+  //['1980', '1990', '2000', '2005', '2010', '2015', '2020'];
+  const labels = dataBack.map(({x}) => x)
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Linha',
+        data: labels.map((x) => coeA*x + coeB),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        type:'line',
+        fill: false,
+        showLine: false, //<- set this
+        pointBackgroundColor: 'black',
+        pointRadius: 5,
+        label: 'Dados',
+        data: labels.map((item,index) => dataBack[index]),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  // console.log("Log || ", data)
+
+
 
   function addNewData(){
+    if(!(newDataX!=' ' && newDataY!=' ' &&newDataX != '' && newDataY != '' && newDataX != undefined && newDataY != undefined)){
+      alert('Coloque Numeros validos.');
+      return;
+    }
     let newDatas = [...dataBack]
     newDatas.push({x:newDataX,y:newDataY});
+    
     setNewDataX(0);
     setNewDataY(0);
     setDataBack(newDatas)
   }
+
+
+  function receber(){
+    
+  }
+
   return (
     <div className='App' style={{flex:1,flexDirection:'row',display:'flex'}}>
       <div style={{flex:1,width:'50%'}}>
@@ -94,23 +113,35 @@ export default function App() {
 
           <label>
             Coeficiente A:
-          <input type="text" value={coefA} onChange={setCoefA} />
+          <input type="text" value={coefA} onChange={(e) => setCoefA(e.target.value)} />
           </label>
           <label>
              Coeficiente B:
-          <input type="text" value={coefB} onChange={setCoefB} />
+          <input type="text" value={coefB} onChange={(e) => setCoefB(e.target.value)} />
           </label>
+          <label>
+             Taxa de Aprendizagem:
+          <input type="text" value={apprentice} onChange={(e) => setApprentice(e.target.value)} />
+          </label>
+          <br/>
           <br/>
           <label>
              Novo Dado X:
-          <input type="text" value={newDataX} onChange={setNewDataX} />
+          <input type="text" value={newDataX} onChange={(e) => setNewDataX(e.target.value)} />
           </label>
+          
           <label>
              Novo Dado Y:
-          <input type="text" value={newDataY} onChange={setNewDataY} />
+          <input type="text" value={newDataY} onChange={e => setNewDataY(e.target.value)} />
           </label>
-          <button  value="Adicionar Dado" onClick={() => addNewData()}>adicionar dado</button>
-
+          <button onClick={() => addNewData()}>Adicionar dado</button>
+          <br/>
+          <br/>
+          <label>
+             Iteracao:
+          <input type="text" value={iterate} onChange={e => setIterate(e.target.value)} />
+          </label>
+          <button onClick={() => receber()}>Receber</button>
         <table>
           <tr><th>Dados</th></tr>
           <tr><th>X</th><th>Y</th></tr>
@@ -119,7 +150,6 @@ export default function App() {
               <tr><td>{item.x}</td><td>{item.y}</td></tr>
             )
           })}
-          <tr><td>10</td><td>9</td></tr>
 
         </table>
       </div>
