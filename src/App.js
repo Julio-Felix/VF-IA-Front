2d00f0e0f412ed3444ae13a74605017e8232ce74
julio_felix_deway <julio.felix@deway.com.br>
2022-05-26 08:57:31 -0300

    Almost Finished

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import faker from 'faker';
-
+import axios from 'axios'
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -49,14 +49,20 @@ const coeB = -388
 export default function App() {
   const [coefA, setCoefA] = useState(0)
   const [coefB, setCoefB] = useState(0)
+  const [coeA, setCoeA] = useState(0.4)
+  const [coeB, setCoeB] = useState(-388)
   const [newDataX, setNewDataX] = useState(0)
   const [newDataY, setNewDataY] = useState(0)
   const [apprentice, setApprentice] = useState(0)
   const [iterate, setIterate] = useState(0)
+  const [response_receive, setResponseReceive] = useState("{'test':'tests'}")
+  const [xPredict,setXpredict] = useState(0)
+  const [xyPredict,setXYpredict] = useState({})
+
 
   const [dataBack, setDataBack] = useState([]);
   //['1980', '1990', '2000', '2005', '2010', '2015', '2020'];
-  const labels = dataBack.map(({x}) => x)
+  const labels = dataBack.map(({xinicial}) => xinicial)
 
   const data = {
     labels,
@@ -74,24 +80,53 @@ export default function App() {
         pointBackgroundColor: 'black',
         pointRadius: 5,
         label: 'Dados',
-        data: labels.map((item,index) => dataBack[index]),
+        data: labels.map((item,index) => {return{x:dataBack[index].xinicial,y:dataBack[index].yinicial}}),
         borderColor: 'rgb(53, 162, 235)',
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+      {
+        type:'line',
+        fill: false,
+        showLine: false, //<- set this
+        pointBackgroundColor: 'black',
+        pointRadius: 5,
+        label: 'Dado Predizer',
+        data: [xyPredict],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(235, 0, 0, 0.5)',
       },
     ],
   };
   // console.log("Log || ", data)
 
-
+  function predizer(){
+    let data = {
+      amostra:dataBack,
+      coefAinicial:coefA,
+      coefBinicial:coefB,
+      taxaDeAprendizagem:apprentice,
+      iteracaoMax:iterate,
+      valorXParaPredizer:xPredict,
+    }
+    console.log('test || ', data)
+    axios.post('http://localhost:8998/v1/predizer/', data).then((response) => {
+      // setResponseReceive(response.data);
+      setXYpredict({x:xPredict,y:response.data.ypredicao})
+      // setCoeA(response.data.coefA)
+      // setCoeB(response.data.coefB)
+    })
+  }
 
   function addNewData(){
     if(!(newDataX!=' ' && newDataY!=' ' &&newDataX != '' && newDataY != '' && newDataX != undefined && newDataY != undefined)){
-      alert('Coloque Numeros validos.');
+      alert('Coloque Numeros validos. obs: campo Y so Aceita Numero.');
       return;
     }
     let newDatas = [...dataBack]
-    newDatas.push({x:newDataX,y:newDataY});
-    
+    newDatas.push({xinicial:newDataX,yinicial:newDataY});
+    newDatas.sort(function(a, b) {
+      return a.xinicial - b.xinicial;
+    });
     setNewDataX(0);
     setNewDataY(0);
     setDataBack(newDatas)
@@ -99,7 +134,19 @@ export default function App() {
 
 
   function receber(){
-    
+    let data = {
+      amostra:dataBack,
+      coefAinicial:coefA,
+      coefBinicial:coefB,
+      taxaDeAprendizagem:apprentice,
+      iteracaoMax:iterate,
+    }
+    console.log('test || ', data)
+    axios.post('http://localhost:8998/v1/criar/', data).then((response) => {
+      setResponseReceive(response.data);
+      setCoeA(response.data.coefA)
+      setCoeB(response.data.coefB)
+    })
   }
 
   return (
@@ -142,15 +189,27 @@ export default function App() {
           <input type="text" value={iterate} onChange={e => setIterate(e.target.value)} />
           </label>
           <button onClick={() => receber()}>Receber</button>
+          <label>
+             X Predicao:
+          <input type="text" value={xPredict} onChange={e => setXpredict(e.target.value)} />
+          </label>
+          <button onClick={() => predizer()}>Predizer</button>
+          
         <table>
           <tr><th>Dados</th></tr>
           <tr><th>X</th><th>Y</th></tr>
           {dataBack.map((item) => {
             return(
-              <tr><td>{item.x}</td><td>{item.y}</td></tr>
+              <tr><td>{item.xinicial}</td><td>{item.yinicial}</td></tr>
             )
           })}
 
+        </table>
+      </div>
+      <div>
+        <table>
+          <tr><th>Response Receber</th></tr>
+          <tr><td>{response_receive}</td></tr>
         </table>
       </div>
     </div>
