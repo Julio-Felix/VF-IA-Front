df953ea6b6ba26640b7bb7c2ec713bc261e7e7d8
rafael-java <71909212+rafael-java@users.noreply.github.com>
2022-06-01 16:23:44 -0300

    Novas alterações conforme documento de novos requisitos

--- src/App.js
+++ src/App.js
@@ -37,16 +37,16 @@ export default function App() {
 
   const [dataBack, setDataBack] = useState([]);
   const [lines, setLines] = useState([]);
-  const [scaleMin, setScaleMin] = useState(-1000)
-  const [scaleMax, setScaleMax] = useState(1000)
-  const [errorscaleMin, setErrScaleMin] = useState(-1000)
-  const [errorscaleMax, setErrScaleMax] = useState(1000)
+  const [scaleMin, setScaleMin] = useState(-1000);
+  const [scaleMax, setScaleMax] = useState(1000);
+  const [errorscaleMin, setErrScaleMin] = useState(-1000);
+  const [errorscaleMax, setErrScaleMax] = useState(1000);
   // const [indices, setIndices] = useState([])
-  const [errosMedios, setErrosMedios] = useState([])
+  const [errosMedios, setErrosMedios] = useState([]);
   //['1980', '1990', '2000', '2005', '2010', '2015', '2020'];
   const labels = dataBack.map(({ xinicial }) => xinicial);
   const indices = errosMedios.map(({ indice }) => indice);
-  console.log("Indicices || ", indices);
+  // console.log("Indicices || ", indices);
   const options = {
     responsive: true,
     scales: {
@@ -87,12 +87,6 @@ export default function App() {
     labels,
     datasets: [
       {
-        label: "Linha",
-        data: labels.map((x) => coeA * x + coeB),
-        borderColor: "rgb(255,0,0)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      {
         type: "line",
         fill: false,
         showLine: false, //<- set this
@@ -103,7 +97,13 @@ export default function App() {
           return { x: dataBack[index].xinicial, y: dataBack[index].yinicial };
         }),
         borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        backgroundColor: "rgba(0, 0, 0, 1)",
+      },
+      {
+        label: "Melhor Linha",
+        data: labels.map((x) => coeA * x + coeB),
+        borderColor: "rgb(255,0,0)",
+        backgroundColor: "rgba(255, 0, 0, 0.7)",
       },
       {
         type: "line",
@@ -111,30 +111,39 @@ export default function App() {
         showLine: false, //<- set this
         pointBackgroundColor: "red",
         pointRadius: 5,
-        label: "Dado Predizer",
+        label: "Predição",
         data: [xyPredict],
         borderColor: "rgb(0, 255, 0)",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        backgroundColor: "rgba(0, 0, 0, 1)",
       },
-      
-      ...lines
+
+      ...lines,
     ],
   };
 
   const dataErrorGraph = {
-    labels:indices,
+    labels: indices,
     datasets: [
       {
         label: "Erros",
         data: indices.map((x, index) => errosMedios[index].erroMedio),
         borderColor: "rgb(0, 0, 0)",
         backgroundColor: "rgba(255, 0, 0, 0.5)",
-      }
-    ]
+      },
+    ],
+  };
+  // console.log("dataErrorGraph || ", dataErrorGraph);
+  // console.log("Log || ", data);
 
+  function resetData() {
+    setDataBack([]);
+    setCoefA(0.0);
+    setCoefB(0.0);
+    setIterate(0);
+    setApprentice(0.0);
+
+    setXpredict(0.0);
   }
-  console.log("dataErrorGraph || ", dataErrorGraph);
-  console.log("Log || ", data)
 
   function preData() {
     setDataBack([
@@ -175,31 +184,47 @@ export default function App() {
       iteracaoMax: iterate,
       valorXParaPredizer: xPredict,
     };
-    console.log("test || ", data);
-    axios.post("https://e636-186-222-173-39.ngrok.io/v1/predizer/", data).then((response) => {
-      // setResponseReceive(response.data);
-      setXYpredict({ x: xPredict, y: response.data.ypredicao });
-      // setCoeA(response.data.coefA)
-      // setCoeB(response.data.coefB)
-    }).catch((err) => {
-      alert(err.response.data.mensagem)
-    });
+    // console.log("test || ", data);
+    axios
+      .post("http://localhost:8998/v1/predizer/", data)
+      .then((response) => {
+        // setResponseReceive(response.data);
+
+        setXYpredict({ x: xPredict, y: response.data.ypredicao });
+        setScaleMax(response.data.escalaDoGraficoPrincipal.ymax);
+        setScaleMin(response.data.escalaDoGraficoPrincipal.ymin);
+        alert(response.data.escalaDoGraficoErros.ymax);
+        // setCoeA(response.data.coefA);
+        // setCoeB(response.data.coefB);
+      })
+      .catch((err) => {
+        alert(err.response.data.mensagem);
+      });
   }
 
   function addNewData() {
-    if (
-      !(
-        newDataX != " " &&
-        newDataY != " " &&
-        newDataX != "" &&
-        newDataY != "" &&
-        newDataX != undefined &&
-        newDataY != undefined
-      )
-    ) {
-      alert("Coloque Numeros validos. obs: campo Y so Aceita Numero.");
+    // if (
+    //   newDataX == " " ||
+    //   newDataY == " " ||
+    //   newDataX == "" ||
+    //   newDataY == "" ||
+    //   newDataX == undefined ||
+    //   newDataY == undefined
+    // ) {
+    //   alert("Valores inválidos");
+    //   return;
+    // }
+
+    if (isNaN(parseFloat(newDataX))) {
+      alert("Valor de X inválido");
       return;
     }
+
+    if (isNaN(parseFloat(newDataY))) {
+      alert("Valor de Y inválido");
+      return;
+    }
+
     let newDatas = [...dataBack];
     newDatas.push({ xinicial: newDataX, yinicial: newDataY });
     newDatas.sort(function (a, b) {
@@ -218,132 +243,92 @@ export default function App() {
       taxaDeAprendizagem: apprentice,
       iteracaoMax: iterate,
     };
-    console.log("test || ", data);
-    axios.post("https://e636-186-222-173-39.ngrok.io/v1/criar/", data).then((response) => {
-      let data = response.data;
-      if(data.estruturaCorreta){
-        setResponseReceive(
-          JSON.stringify(data.estruturaCorreta.erroMedio)
-        );
-        setCoeA(data.estruturaCorreta.coefA);
-        setCoeB(data.estruturaCorreta.coefB);
-      }
-      let newLines = data.coefsLinhas.map((item) => {
-        return {
-          label: "Linha-" + item.indice,
-          data: labels.map((x) => item.coefA * x + item.coefB),
-          borderColor: "rgb(0, 0, 0)",
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-        }
+    axios
+      .post("http://localhost:8998/v1/criar/", data)
+      .then((response) => {
+        setCoeA();
+        setCoeB();
+        let data = response.data;
+        let newLines = data.coefsLinhas.map((item) => {
+          return {
+            label: item.indice,
+            data: labels.map((x) => item.coefA * x + item.coefB),
+            borderColor: "rgb(0, 0, 0)",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          };
+        });
+        // setLines(newLines);
+        recursiveLines(newLines, 0, [], data);
+        setScaleMax(data.escalaDoGraficoPrincipal.ymax);
+        setScaleMin(data.escalaDoGraficoPrincipal.ymin);
+        setErrScaleMax(data.escalaDoGraficoErros.ymax);
+        setErrScaleMin(data.escalaDoGraficoErros.ymin / 1000);
+        setErrosMedios(data.erros);
       })
-      // setLines(newLines);
-      recursiveLines(newLines, 0, []);
-      setScaleMax(data.escalaDoGraficoPrincipal.ymax);
-      setScaleMin(data.escalaDoGraficoPrincipal.ymin / 10000);
-      setErrScaleMax(data.escalaDoGraficoErros.ymax)
-      setErrScaleMin(data.escalaDoGraficoErros.ymin)
-      setErrosMedios(data.erros);
-      
-    }).catch((err) => {
-      alert(err.response.data.mensagem)
-    });
+      .catch((err) => {
+        alert(err.response.data.mensagem);
+      });
   }
 
-  function recursiveLines(coefsLinhas, i, oldLines) {
-    let newLines = [...oldLines,coefsLinhas[i]]
-    setLines(newLines);
+  function recursiveLines(coefsLinhas, i, oldLines, data) {
+    let newLines = [...oldLines, coefsLinhas[i]];
     setTimeout(() => {
-      if(coefsLinhas.length > i+1){
-        recursiveLines(coefsLinhas, i+1, newLines)
+      if (data.estruturaCorreta && i + 1 == data.estruturaCorreta.indice) {
+        // setResponseReceive(JSON.stringify(data.estruturaCorreta.erroMedio));
+        setCoeA(data.estruturaCorreta.coefA);
+        setCoeB(data.estruturaCorreta.coefB);
+        setLines(newLines);
+        recursiveLines(coefsLinhas, i + 1, newLines, data);
+      } else if (coefsLinhas.length > i) {
+        setLines(newLines);
+        recursiveLines(coefsLinhas, i + 1, newLines, data);
+      } else if (coefsLinhas.length == i) {
+        setLines(newLines);
+        let newLines = [...oldLines, coefsLinhas[i]];
       }
-      
-    },5000)
+    }, 500);
   }
 
   return (
     <div className="App">
-      <div
-        style={{ flex: 1, flexDirection: "row", display: "flex" }}
-      >
+      <div style={{ flex: 1, flexDirection: "row", display: "flex" }}>
         <div style={{ flex: 1, width: "50%" }}>
           <Line options={options} data={data} />
         </div>
 
         <div style={{ flex: 1, width: "100%" }}>
           {/* <div> */}
-          <div style={{ flex: 1, width: "100%", margin:10 }}>
-            <label style={{}}>
-              Coeficiente A:
-              <input
-                type="text"
-                value={coefA}
-                onChange={(e) => setCoefA(e.target.value)}
-              />
-            </label>
-          </div>
-          <div style={{ flex: 1, width: "100%", margin:10 }}>
-            <label>
-              Coeficiente B:
-              <input
-                type="text"
-                value={coefB}
-                onChange={(e) => setCoefB(e.target.value)}
-              />
-            </label>
-          </div>
-          <label>
-            Taxa de Aprendizagem:
-            <input
-              type="text"
-              value={apprentice}
-              onChange={(e) => setApprentice(e.target.value)}
-            />
-          </label>
-          <br />
-          <br />
-          <label>
-            Novo Dado X:
-            <input
-              type="text"
-              value={newDataX}
-              onChange={(e) => setNewDataX(e.target.value)}
-            />
-          </label>
-
-          <label>
-            Novo Dado Y:
-            <input
-              type="text"
-              value={newDataY}
-              onChange={(e) => setNewDataY(e.target.value)}
-            />
-          </label>
-          <button onClick={() => addNewData()}>Adicionar dado</button>
-          <br />
-          <br />
-          <label>
-            Iteracao:
-            <input
-              type="text"
-              value={iterate}
-              onChange={(e) => setIterate(e.target.value)}
-            />
-          </label>
-          <button onClick={() => receber()}>Receber</button>
-          <label>
-            X Predicao:
-            <input
-              type="text"
-              value={xPredict}
-              onChange={(e) => setXpredict(e.target.value)}
-            />
-          </label>
-          <button onClick={() => predizer()}>Predizerrr</button>
-          <br />
           <br />
           <button onClick={() => preData()}>Valores Padrao</button>
+          <span> </span>
           <button onClick={() => preData2()}>Valores Padrao 2</button>
-          
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <label>
+              Dado X:
+              <span> </span>
+              <input
+                type="text"
+                value={newDataX}
+                onChange={(e) => setNewDataX(e.target.value)}
+              />
+            </label>
+          </div>
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <label>
+              Dado Y:
+              <span> </span>
+              <input
+                type="text"
+                value={newDataY}
+                onChange={(e) => setNewDataY(e.target.value)}
+              />
+            </label>
+          </div>
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <button onClick={() => addNewData()}>Adicionar dado</button>
+            <span> </span>
+            <button onClick={() => resetData()}>Resetar dados</button>
+          </div>
           <table>
             <tr>
               <th>Dados</th>
@@ -361,22 +346,101 @@ export default function App() {
               );
             })}
           </table>
+          <br />
+          <div
+            style={{ flex: 1, width: "100%", height: "5px", margin: 1 }}
+          ></div>
+          <div style={{ flex: 1, width: "50%", margin: 10 }}>
+            <label style={{}}>
+              Coeficiente A:
+              <span> </span>
+              <input
+                type="text"
+                value={coefA}
+                onChange={(e) => setCoefA(e.target.value)}
+              />
+            </label>
+          </div>
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <label>
+              Coeficiente B:
+              <span> </span>
+              <input
+                type="text"
+                value={coefB}
+                onChange={(e) => setCoefB(e.target.value)}
+              />
+            </label>
+          </div>
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <label>
+              Taxa de
+              <br />
+              Aprendizagem:
+              <span> </span>
+              <input
+                type="text"
+                value={apprentice}
+                onChange={(e) => setApprentice(e.target.value)}
+              />
+            </label>
+          </div>
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <label>
+              Iteração Máxima:
+              <span> </span>
+              <input
+                type="text"
+                value={iterate}
+                onChange={(e) => setIterate(e.target.value)}
+              />
+            </label>
+          </div>
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <button onClick={() => receber()}>Receber</button>
+          </div>
+          <div
+            style={{ flex: 1, width: "100%", height: "5px", margin: 1 }}
+          ></div>
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <label>
+              X Predicao:
+              <span> </span>
+              <input
+                type="text"
+                value={xPredict}
+                onChange={(e) => setXpredict(e.target.value)}
+              />
+            </label>
+          </div>
+          <div style={{ flex: 1, width: "100%", margin: 10 }}>
+            <button onClick={() => predizer()}>Predizer</button>
+          </div>
+          <div
+            style={{ flex: 1, width: "100%", height: "5px", margin: 1 }}
+          ></div>{" "}
         </div>
       </div>
       <div
-        style={{ flex: 1, flexDirection: "row", display: "flex" }}
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          display: "flex",
+        }}
       >
-        <div style={{ flex: 1, width: "50%" }}>
+        <div style={{ flex: 1, width: "50%", height: "1000px" }}>
           <Line options={optionsErrorGraph} data={dataErrorGraph} />
         </div>
         <div style={{ flex: 1, width: "100%" }}>
           <table>
             <tr>
-              <th>Response Receber</th>
+              <th>Erros Médios</th>
             </tr>
             {errosMedios.map((item) => {
               return (
-                <tr style={!item.subindo ? {color:'black'}:{color:'red'}}>
+                <tr
+                  style={!item.subindo ? { color: "black" } : { color: "blue" }}
+                >
                   <td>{item.indice}</td>
                   <td>{item.erroMedio}</td>
                 </tr>
@@ -386,6 +450,5 @@ export default function App() {
         </div>
       </div>
     </div>
-    
   );
 }
